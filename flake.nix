f4caf0352970dfd6a1f1cfd14196e8528d1f10d6
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-12-01 20:35:01 -0300

    Creating devshell with a flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Advent of Code 2024";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in with pkgs; {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          zig
+        ];
+      };
+    });
+}
