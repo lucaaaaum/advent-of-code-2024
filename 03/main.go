f588f5018ceabda1cdacd3a16d949f11e9cdc33e
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-12-03 13:24:35 -0300

    Reading input

--- 03/main.go
+++ 03/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+    input, err := readInput()
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("input: %v\n", input)
+}
+
+func readInput() (string, error) {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		return "", err
+	}
+
+	scanner := bufio.NewScanner(file)
+
+    input := ""
+
+    for scanner.Scan() {
+        input += scanner.Text()
+    }
+
+    return input, nil
 }
